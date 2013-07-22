cd07e206a2ba31ee5db4a7a8bb168c45bb97c288
Chenoa Siegenthaler <cowmanifestation@gmail.com>
2013-07-22 16:20:31 -0500

    Integration tests

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "About page" do
+    it "should have h1 'About Chenoa'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Chenoa')
+    end
+
+    it "should have title 'About Chenoa'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Chenoa Siegenthaler | About")
+    end
+  end
+
+  describe "Resume page" do
+    it "should have h1 'Resume'" do
+      visit '/static_pages/resume'
+      page.should have_selector('h1', :text => 'Resume')
+    end
+
+    it "should have title 'Resume'" do
+      visit '/static_pages/resume'
+      page.should have_selector('title', :text => "Chenoa Siegenthaler | Resume")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Chenoa Siegenthaler | Contact")
+    end
+  end
+end
