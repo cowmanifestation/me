7ab3400b03bb69007b35c333008828958bb958e5
Chenoa Siegenthaler <cowmanifestation@gmail.com>
2013-07-22 17:39:42 -0500

    Refactored tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,37 +3,37 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "About page" do
+    before { visit about_path }
+
     it "should have h1 'About Chenoa'" do
-      visit about_path
       page.should have_selector('h1', :text => 'About Chenoa')
     end
 
     it "should have title 'About Chenoa'" do
-      visit about_path
       page.should have_selector('title', :text => "Chenoa Siegenthaler | About")
     end
   end
 
   describe "Resume page" do
+    before { visit resume_path }
+
     it "should have h1 'Resume'" do
-      visit resume_path
       page.should have_selector('h1', :text => 'Resume')
     end
 
     it "should have title 'Resume'" do
-      visit resume_path
       page.should have_selector('title', :text => "Chenoa Siegenthaler | Resume")
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
+
     it "should have h1 'Contact'" do
-      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have title 'Contact'" do
-      visit contact_path
       page.should have_selector('title', :text => "Chenoa Siegenthaler | Contact")
     end
   end
